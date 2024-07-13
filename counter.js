371d6c21ae3054050a95399e2dc2b0244ca082b9
sss <>
2024-07-13 19:08:30 +0900

    Further JavaScript text has been added

--- counter.js
+++ counter.js
@@ -3,10 +3,11 @@
     const $image = document.getElementById("js-image");
     const $imageAlt = document.getElementById("js-image-alt");
 
-
     const clickHandler = (e) => {
         const $targetButton = e.currentTarget;
         let currentCount = parseInt($counter.textContent);
+        const step = parseInt($targetButton.getAttribute('data-step')) || 0;
+
         if ($targetButton.textContent === "+") {
             $counter.textContent = currentCount + 1;
         } else if ($targetButton.textContent === "-") {
@@ -15,17 +16,18 @@
             $counter.textContent = currentCount + 10;
         } else if ($targetButton.textContent === "-10") {
             $counter.textContent = currentCount - 10;
+        } else if (step !== 0) {
+            $counter.textContent = currentCount + step;
         }
 
-         //カウントが3の倍数に達したら画像とテキストを表示
-         if (parseInt($counter.textContent) % 3 === 0 && parseInt($counter.textContent) !== 0) {
+        //カウントが3の倍数に達したら画像とテキストを表示
+        if (parseInt($counter.textContent) % 3 === 0 && parseInt($counter.textContent) !== 0) {
             $image.style.display = "block";
             $imageAlt.style.display = "block";
         } else {
             $image.style.display = "none";
             $imageAlt.style.display = "none";
         }
-
     }
 
     for (let index = 0; index < document.getElementsByClassName("js-button").length; index++) {
